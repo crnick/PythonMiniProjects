0638ffbb4ab8c8745fd9f51c60923dcc3f94bb58
nikhil joglekar <nikhil.joglekar@ideaqu.com>
2025-07-09 06:27:24 +0530

    Put calculation logic inside separate function

--- calculator.py
+++ calculator.py
@@ -4,18 +4,21 @@ operator = input ("Enter an operator (+ - * /): ")
 num1 = float(input ("Enter the 1st number: "))
 num2 = float(input ("Enter the 2nd number: "))
 
+def handleOperations():
+	if operator == "+":
+		result = num1 + num2 
+		print(round(result),3)
+	elif operator == "-":
+		result = num1 - num2 
+		print(round(result),3)
+	elif operator == "*":
+		result = num1 * num2
+		print(round(result),3)
+	elif operator == "/":
+		result = num1 / num2
+		print(round(result),3)
+	else:
+		print(f"{operator} is not a valid operator")
 
-if operator == "+":
-	result = num1 + num2 
-	print(round(result),3)
-elif operator == "-":
-	result = num1 - num2 
-	print(round(result),3)
-elif operator == "*":
-	result = num1 * num2
-	print(round(result),3)
-elif operator == "/":
-	result = num1 / num2
-	print(round(result),3)
-else:
-	print(f"{operator} is not a valid operator")
+if __name__ == "__main__":
+	handleOperations()
