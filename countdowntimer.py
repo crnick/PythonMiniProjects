2747d6390f9d66404785247932305fa94709ff13
nikhil joglekar <nikhil.joglekar@ideaqu.com>
2025-07-22 21:59:07 +0530

    added a if condition for handling negative numbering and updated the range values

--- countdowntimer.py
+++ countdowntimer.py
@@ -1,15 +1,16 @@
 import time 
 
+my_time = int (input("Enter the time in seconds: "))
 
-
-my_time = int (input("Enter the time in seconds"))
-
-for x in range(my_time,0,-1): #count backwards
-	seconds = x % 60
-	minutes = int (x/60)%60
-	ghours = int (x / 3600)
-	print(f"{hours:02}:{minutes:02}:{seconds:02}")
-	time.sleep(1)
+if my_time <= 0:
+    print("Time must be positive.")
+else:
+	for x in range(my_time,-1,-1): #count backwards
+		seconds = x % 60
+		minutes = int (x/60)%60
+		hours = int (x / 3600)
+		print(f"{hours:02}:{minutes:02}:{seconds:02}")
+		time.sleep(1)
 
 
 print("Time's up")
